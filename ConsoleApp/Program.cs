65e37954ce00c77b9e5391f3675ccdb357f54661
Владислав Цуркан <c.vladd@mail.ru>
2020-06-19 13:50:14 +0300

    implemented EvenSum method;

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp
 {
     static class Program
     {
+        static void EvenSum()
+        {
+
+        }
         static void Main()
         {
             Console.WriteLine("75group");
