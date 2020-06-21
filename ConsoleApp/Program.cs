dbc0a78881f4a54f7887d87c7a814266c2e02108
Владислав Цуркан <c.vladd@mail.ru>
2020-06-21 16:26:05 +0300

    my code displays the sum of all primes from 0 to the number specified by the user

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -21,9 +21,26 @@ namespace ConsoleApp
             Console.WriteLine("The sum of the even numbers is {0}", sum);
             Console.ReadKey(true);
         }
+        /// <summary>
+        /// my code displays the sum of all primes from 0 to the number specified by the user
+        /// </summary>
+        static void PrimeSum()
+        {
+            int sum=0;
+            int i;
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (i=0; i<=n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of all primes {0}", sum);
+            Console.ReadKey(true);
+        }
         static void Main()
         {
-            EvenSum();
+            //EvenSum();
+            PrimeSum();
             Console.WriteLine("75group");
         }
     }
