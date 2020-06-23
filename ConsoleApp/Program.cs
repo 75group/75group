f515a63d5b881173b41ff4685653c4761e7acebb
Владислав Цуркан <c.vladd@mail.ru>
2020-06-23 18:06:06 +0300

    Added a method determining whether the number is prime. The cycle that displays the sum of all primes in the range from 2 to the number entered by the user

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -27,15 +27,38 @@ namespace ConsoleApp
         static void PrimeSum()
         {
             int sum=0;
-            int i;
-            Console.WriteLine("Enter the number");
+            Console.Write("Enter the number: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            for (i=0; i<=n; i++)
+            for (int i = 2; i <= n; i++)
             {
-                sum += i;
+                if (IsSimple(i)==true)
+                {
+                    sum += i;
+                }
             }
             Console.WriteLine("The sum of all primes {0}", sum);
-            Console.ReadKey(true);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// this method determines is a prime number
+        /// </summary>
+        /// <param name="a">in number</param>
+        /// <returns></returns>
+        static bool IsSimple (int a)
+        {
+            int kd = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                if (a%i==0)
+                {
+                    kd++;
+                }
+            }
+            if (kd == 2)
+            {
+                return true;
+            }
+            else return false;
         }
         static void Main()
         {
