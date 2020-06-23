45eb19e4ff3b9f47812eda3bfd7038f14b313776
W_hit_e <pbelous@progredev.com>
2020-06-23 12:31:32 +0700

    preimplement

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp
 {
@@ -6,7 +9,7 @@ namespace ConsoleApp
     {
         static void Main()
         {
-            EvenSum();
+            Password();
         }
         /// <summary>
         /// print gapful numbers to console from range 100 to 999
@@ -69,6 +72,73 @@ namespace ConsoleApp
                 }
             }
             while (!isCorrectFormatNumber);
+        }
+        /// <summary>
+        /// output to the console prime numbers of input number and input sum prime numders
+        /// </summary>
+        public static void PrimaNumber()
+        {
+            bool isPrimaNumber;
+            do
+            {
+                Console.WriteLine("Enter the number to which you need to find prime numbers (number mast be > 1)");
+                try
+                {
+                    isPrimaNumber = true;
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    int variable;
+                    int sum = 0;
+                    for (int inputNumber = 0; inputNumber <= number; inputNumber++)
+                    {
+                        variable = 0;
+                        for (int x = 1; x <= number; x++)
+                        {
+                            if (inputNumber % x == 0)
+                            {
+                                variable++;
+                            }
+                        }
+                        if (variable == 2)
+                        {
+                            sum += inputNumber;
+                            Console.WriteLine($"{inputNumber}");
+                        }
+                    }
+                    Console.WriteLine($"Sum all prime numbers = {sum}");
+                }
+                catch (Exception e)
+                {
+                    isPrimaNumber = false;
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (!isPrimaNumber);
+        }
+        /// <summary>
+        /// method create passwopd and checks its compliance
+        /// </summary>
+        public static void Password()
+        {
+            Regex regex = new Regex(@"^[a-zA-Z0-9]");
+            Console.WriteLine("input your password");
+            string password = Console.ReadLine();
+
+            if(password.Length < 5)
+            {
+                Console.WriteLine("Sorry your password most be at least 5 characters");
+            }
+            else if (password.Length > 10)
+            {
+                Console.WriteLine("Sorry your password most be no more than 10 characters");
+            }
+            foreach(char sr in password)
+            {
+                if()
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
